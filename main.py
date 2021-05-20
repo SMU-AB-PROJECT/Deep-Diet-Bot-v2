265a36f2280e56e8895a9ca8e711e9e26b62ce34
yoonho <yoonho0922@naver.com>
2021-05-20 17:43:08 +0900

    feat: eat time and food name database save

--- main.py
+++ main.py
@@ -4,27 +4,38 @@
 
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler  # import modules
 import os
-from load_model import load_model,predict_output,training_test
+from load_model import load_model,predict_output
 import tensorflow as tf
+import DBModel
+import time
 
-'''
-파일이 저장될 directory: 환경에 맞는 경로 수정 필요
-'''
-path = 'C:\\files\\'
+'''파일이 저장될 directory: 환경에 맞는 경로 수정 필요'''
+path = 'C:\\deepdiet-files\\'
 dir_now = os.path.dirname(os.path.abspath(path))  # real path to dirname
 
+'''DB 모델 생성'''
+dbm = DBModel.DBModel()
 
-# message reply function
+'''전역변수 정의'''
+eatTime='처음'
+print('변수가 재정의 됨')
+
+'''Handler 정의'''
+# reply username
 def get_message(update, context) :
-    update.message.reply_text("got text")
-    update.message.reply_text(update.message.text)
+    global eatTime
+    req = update.message.text
+    print('request: ' + req)
 
+    print('get eat time: {}'.format(eatTime))
+    if req == '아침' or '점심' or '저녁':
+        eatTime = req
+        update.message.reply_text("{}에 먹은 음식 사진을 보내주세요.".format(eatTime))
+        print('get eat time: {}'.format(eatTime))
+    else:
+        update.message.reply_text("hello " + update.message.from_user.first_name)
+        print('get eat time: {}'.format(eatTime))
 
-# help reply function
-def help_command(update, context) :
-    update.message.reply_text("무엇을 도와드릴까요?")
-
-import time
 # photo reply function
 def get_photo(update, context) :
     file_path = os.path.join(path, 'from_telegram.png')
@@ -33,40 +44,54 @@ def get_photo(update, context) :
     photo_file.download(file_path)
     #update.message.reply_text('photo saved')
     time.sleep(1.)
+
+    global eatTime
+    print('먹은 시간: ' + eatTime)
     if os.path.isfile(path + 'from_telegram.png'):
         # label = labels_list[predict_output(model, path+'from_telegram.png')]
-        label = labels_list[0]
-        update.message.reply_text('음식은 '+ label+'입니다')
+        foodname = labels_list[0]
+        update.message.reply_text('{}에 먹은 음식은 {}입니다.'.format(eatTime, foodname))
+        print(eatTime)
+
+        username = update.message.from_user.first_name
+
+        if eatTime == '아침':
+            dbm.insert(username, '2021-05-21', 'breakfast', foodname)
+            print('database 저장 완료.')
+        elif eatTime == '점심':
+            dbm.insert(username, '2021-05-21', 'lunch', foodname)
+            print('database 저장 완료.')
+        elif eatTime == '저녁':
+            dbm.insert(username, '2021-05-21', 'dinner', foodname)
+            print('database 저장 완료.')
 
     else:
         update.message.reply_text('다시 보내주세요.')
         print('Image is not found!')
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     api_key = '1760236283:AAFcjOBmTOM8MPWNUZrhDfGN52-ToK4YAfw'
-    #labels_list = os.listdir('dataset')
-    labels_list = ['콜라','라면','김밥','스파게티']
-    #model = training_test()
-    #model = tf.keras.models.load_model('model/testing_model/res.h5')
+    labels_list = ['Grilled Mackerel', 'bibimbap', 'fish sushi', 'fruit salad', 'gimbap', 'jajangmyeon', 'miyeokguk',
+                   'ramen','seolleongtang', 'spaghetti', 'tteokbokki', 'tteokguk']
+    # model = load_model()
     print('start telegram chat bot')
 
     updater = Updater(api_key, use_context=True)
 
-    message_handler = MessageHandler(Filters.text & (~Filters.command), get_message)  # 메세지중에서 command 제외
+    '''handler 매칭'''
+    # 메시지 응답
+    message_handler = MessageHandler(Filters.text & (~Filters.command), get_message)
     updater.dispatcher.add_handler(message_handler)
 
-    help_handler = CommandHandler('help', help_command)
-    updater.dispatcher.add_handler(help_handler)
-
+    # 사진 파일 응답
     photo_handler = MessageHandler(Filters.photo, get_photo)
     updater.dispatcher.add_handler(photo_handler)
 
     updater.start_polling(timeout=3, clean=True)
     updater.idle()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
